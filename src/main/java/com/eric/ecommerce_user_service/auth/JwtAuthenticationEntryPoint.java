e27564a57ffe21806ceebb436bfccd4988aea86e
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-02-26 21:18:34 +0100

    feat(security): Add JwtAuthenticationEntryPoint for unauthorized requests
    Handles unauthorized requests by returning appropriate HTTP status codes.
    Ensures proper error response for unauthenticated API access.

---
+++ src/main/java/com/eric/ecommerce_user_service/auth/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.eric.ecommerce_user_service.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * this will handle unauthorized access by returning a 401 Unauthorized response.
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Invalid token");
+    }
+}
