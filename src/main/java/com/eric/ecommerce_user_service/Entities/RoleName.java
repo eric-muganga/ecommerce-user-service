28dd5b47afe3f735775011b76501ef10e35b1499
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-03-02 11:28:12 +0100

    chore(Enum): Define possible user roles (ROLE_USER, ROLE_ADMIN, ROLE_SELLER,ROLE_MANAGER,ROLE_SUPPORT, ROLE_DELIVERY)

---
+++ src/main/java/com/eric/ecommerce_user_service/Entities/RoleName.java
@@ -0,0 +1,10 @@
+package com.eric.ecommerce_user_service.Entities;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_SELLER,
+    ROLE_MANAGER,
+    ROLE_SUPPORT,
+    ROLE_DELIVERY
+}
